c359a64cc4f73b5c84da87b2c958bca6a8228285
Andreas Nedbal <git@pixelde.su>
2021-09-05 20:15:03 +0200

    :sparkles: add Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,16 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: [
-    '@typescript-eslint'
+    '@typescript-eslint',
+    'prettier'
   ],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
   rules: {
+    'prettier/prettier': 'warn',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off'
   }
